57f1b9ffd0a5f5e2ff82d8054b8a089184efaa60
kotkits <bumataykiko@gmail.com>
2025-08-04 22:15:14 +0800

    qwe

--- chatbot-frontend/pages/api/accounts/[id]/admins.js
+++
@@ -1,8 +0,0 @@
-// returns Facebook‐page admins for account
-export default async function handler(req, res) {
-  const { id } = req.query;
-  // TODO: verify user has access to account
-  const admins = await fetchPageAdmins(id); 
-  // returns [{ name, email, avatar }]
-  res.status(200).json(admins);
-}
